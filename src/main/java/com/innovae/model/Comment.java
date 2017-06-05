30c4367d08d961cb51a0c4954e2e70c72d70801c
manson <Manson3m3@users.noreply.github.com>
2017-06-05 23:23:57 +0800

    i forget create comment model- -

---
+++ src/main/java/com/innovae/model/Comment.java
@@ -0,0 +1,68 @@
+package com.innovae.model;
+
+import java.util.Date;
+
+
+public class Comment {
+    private Integer id;
+
+    private Integer topicId;
+
+    private String userId;
+
+    private Date time;
+
+    private String content;
+
+    public Comment(Integer id, Integer topicId, String userId, Date time, String content) {
+        this.id = id;
+        this.topicId = topicId;
+        this.userId = userId;
+        this.time = time;
+        this.content = content;
+    }
+
+    public Comment() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(Integer topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
