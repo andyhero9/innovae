960efaf0e3c20dcf1302e15d103c18c8f9b9fbae
manson3m3 <manson3m3@gmail.com>
2017-06-09 20:07:50 +0800

    merge forum and comment

--- src/main/java/com/innovae/controller/ForumController.java
+++ src/main/java/com/innovae/controller/ForumController.java
@@ -1,5 +1,6 @@
 package com.innovae.controller;
 
+import com.innovae.model.Comment;
 import com.innovae.model.Topic;
 import com.innovae.service.ForumService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +37,7 @@ public class ForumController {
      * @throws Exception
      */
     @ResponseBody
-    @RequestMapping(value = "addTopic",method = {RequestMethod.GET,RequestMethod.POST})
+    @RequestMapping(value = "/addTopic",method = {RequestMethod.GET,RequestMethod.POST})
     public boolean addTopic(Topic topic,HttpServletRequest request,HttpServletResponse response)throws Exception
     {
         topic.setUserId(request.getSession().getAttribute("id").toString());
@@ -45,4 +46,22 @@ public class ForumController {
         return forumService.addTopic(topic);
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/addComment",method = {RequestMethod.GET,RequestMethod.POST})
+    public boolean addComment(Comment comment,HttpServletRequest request,HttpServletResponse response) throws Exception
+    {
+        //int TopicId
+        int temp = 0;
+        comment.setTopicId(temp);
+        comment.setUserId(request.getSession().getAttribute("id").toString());
+        //after add a comment
+        response.setHeader("refresh","1");
+        return forumService.addComment(comment);
+    }
+    @ResponseBody
+    @RequestMapping(value = "/viewTopic",method ={RequestMethod.GET,RequestMethod.POST})
+    public String getCommentsList(int topicId,int page, HttpServletRequest request,HttpServletResponse response)
+    {
+        return forumService.getCommentListJson(topicId,page);
+    }
 }
--- src/main/java/com/innovae/service/ForumService.java
+++ src/main/java/com/innovae/service/ForumService.java
@@ -1,5 +1,6 @@
 package com.innovae.service;
 
+import com.innovae.model.Comment;
 import com.innovae.model.Topic;
 
 /**
@@ -9,4 +10,7 @@ public interface ForumService {
     String getTopicListJson(int type, int curNum, int perPage, int isSortByHot);
     //by manson
     boolean addTopic(Topic topic) throws Exception;
+
+    boolean addComment(Comment comment) throws Exception;
+    String getCommentListJson(int topicId,int page);
 }
